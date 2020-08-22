88d05527859e620c0433d6d6286d4cfd085189af
Denis <novikovdawork@yandex.ru>
2020-08-22 18:34:52 +0700

    Добавил eslint, устранил замечания

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2020": true
+    },
+    "extends": "standard",
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+        "no-extra-semi": "error",
+        "semi": [2, "always"]
+    }
+};
