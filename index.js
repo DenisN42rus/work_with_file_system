da7e5a406ba17278429cd3364967a6dbd34c5669
Denis <novikovdawork@yandex.ru>
2020-08-30 11:27:01 +0700

    async\await

--- index.js
+++ index.js
@@ -1,83 +1,83 @@
 const path = require('path');
-const fs = require('fs');
-const Watcher = require('./watcher.js');
+const fs = require('fs/promises');
 const errorHandler = require('./utils/errorHendler.js');
 const program = require('./utils/commander.js');
 
 program.parse(process.argv);
 
 const oldDirectory = program.folder;
-const newDirectory = program.output
+const newDirectory = program.output;
 
-const watcher = new Watcher(() => {
-  console.log('Sorting complete. We can remove source folder');
-  if (program.delete) {
-    deleteDir();
+const isAccessible = (path) => {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false);
+};
+
+const createFolderIsNotExist = async () => {
+  if (!(await isAccessible(oldDirectory))) {
+    errorHandler(`Not found source folder: ${oldDirectory}`);
   }
-});
 
-if (!fs.existsSync(oldDirectory)) {
-  errorHandler(`Not found source folder: ${oldDirectory}`);
-}
-
-if (!fs.existsSync(newDirectory)) {
-  fs.mkdirSync(newDirectory);
-}
-
-const makeDir = (dir) => {
-  if (!fs.existsSync(`${newDirectory}/${dir}`)) {
-    fs.mkdirSync(`${newDirectory}/${dir}`);
+  if (!(await isAccessible(newDirectory))) {
+    await fs.mkdir(newDirectory);
   }
 };
 
-const moveFile = (dir, file) => {
+const makeDir = async (dir) => {
+  if (!(await isAccessible(`${newDirectory}/${dir}`))) {
+    await fs.mkdir(`${newDirectory}/${dir}`);
+  }
+};
+
+const moveFile = async (dir, file) => {
   const firstChar = file[0].toUpperCase();
   const oldPath = path.join(dir, file);
   const newPath = path.join(newDirectory, firstChar, file);
 
-  makeDir(firstChar);
-  watcher.startProccess(dir)
-  fs.copyFile(oldPath, newPath, err => {
-    if (err) {
-      errorHandler(err);
-      return;
-    }
-    watcher.endProccess(dir)
-  });
+  try {
+    await makeDir(firstChar);
+    await fs.copyFile(oldPath, newPath);
+    console.log(file);
+  } catch (err) {
+    errorHandler(err);
+  }
 };
 
-const readDir = (dir) => {
-  watcher.started()
-  watcher.startProccess(dir)
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      errorHandler(err);
-      return;
+const readDir = async (dir) => {
+  const files = await fs.readdir(dir);
+  for (const item of files) {
+    const dirPath = path.join(dir, item);
+    const state = await fs.stat(dirPath);
+    if (state.isDirectory()) {
+      await readDir(dirPath);
+    } else {
+      await moveFile(dir, item);
     }
-
-    for (const item of files) {
-      const dirPath = path.join(dir, item);
-      const state = fs.statSync(dirPath);
-      if (state.isDirectory()) {
-        readDir(dirPath);
-      } else {
-        moveFile(dir, item);
-      }
-    }
-
-    watcher.endProccess(dir)
-  });
+  }
 };
 
-const deleteDir = () => {
-  fs.rmdir(oldDirectory, {
+const deleteDir = async () => {
+  await fs.rmdir(oldDirectory, {
     recursive: true
-  }, err => {
-    if (err) {
-      errorHandler(err);
-      return;
-    }
+  }).catch(err => {
+    errorHandler(err);
   });
 };
 
-readDir(oldDirectory);
+const sorting = async () => {
+  try {
+    await readDir(oldDirectory);
+    console.log('Sorting complete. We can remove source folder');
+
+    if (program.delete) {
+      deleteDir();
+    }
+  } catch (err) {
+    errorHandler(err);
+  }
+};
+
+createFolderIsNotExist();
+sorting();
